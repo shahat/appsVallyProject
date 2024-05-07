beff33764ccf243e4a55e9bd99e8400106e58ee9
shahat <me6847570@gmail.com>
2024-05-07 18:13:37 +0300

    finished backend connection

--- client/build/img/aboutUsIcons/aboutCompanyIcons.js
+++
@@ -1,6 +0,0 @@
-import officeImage from "./office-building.png";
-import serviceImage from "./service.png";
-export const aboutCompanyIcons = {
-    officeImage,
-    serviceImage,
-}
--- client/build/img/svg/svgImgs.js
+++
@@ -1,8 +0,0 @@
-import form from "./from.svg";
-import softwareIntration from "./Software integration.svg";
-import invoice from "./invoice-animate.svg";
-export const svgImgs = {
-  form,
-  softwareIntration,
-  invoice,
-};
